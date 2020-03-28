a7674c1f3de87a748ecdaafb93eeb851c0fcf5d9
Garry A Offord <gofford@accertify.com>
2020-03-28 14:50:32 -0500

    playing with flit vs setup

--- src/biglib/__init__.py
+++ src/biglib/__init__.py
@@ -4,7 +4,7 @@ Custom library used by my tools.
 Library contains a bunch of tools for interceptas, vertigo, and miscellaneous purposes.
 """
 
-__version__ = "1.5.9"
+__version__ = "1.5.10"
 
 import logging
 import os
