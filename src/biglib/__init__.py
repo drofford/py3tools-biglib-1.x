62ae57cb9f9a883dcb5992df7b06b798892c59e2
Garry A Offord <gofford@accertify.com>
2020-04-13 12:03:41 -0500

    Bump version: 2.0.1-6 → 2.0.1-7

--- src/biglib/__init__.py
+++ src/biglib/__init__.py
@@ -4,7 +4,7 @@ Custom library used by my tools.
 Library contains a bunch of tools for interceptas, vertigo, and miscellaneous purposes.
 """
 
-__version__ = "2.0.1-6-0"
+__version__ = "2.0.1-7-0"
 
 import logging
 import os
