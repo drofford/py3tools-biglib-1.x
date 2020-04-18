d2ffa12df1312446beeb79811416a6043415244b
Garry A Offord <gofford@accertify.com>
2020-04-17 19:35:28 -0500

    added join method to text_utils

--- src/biglib/utils/text_utils.py
+++ src/biglib/utils/text_utils.py
@@ -10,12 +10,12 @@ def pluralize(txt, /, verbs=[], nouns=[], count=0):
                         {noun}
                         {count}.
 
-        verbs       this is a tuple containing (the plural verb form, the singular verb form). If it contains 
-                    only one value, that value will be for both plural and singular forms. It can also be a 
+        verbs       this is a tuple containing (the plural verb form, the singular verb form). If it contains
+                    only one value, that value will be for both plural and singular forms. It can also be a
                     string rather than a tuple, in which case will used be for both plural and singular forms.
 
-        nouns       this is a tuple containing (the plural noun form, the singular noun form). If it contains 
-                    only one value, that value will be for both plural and singular forms. It can also be a 
+        nouns       this is a tuple containing (the plural noun form, the singular noun form). If it contains
+                    only one value, that value will be for both plural and singular forms. It can also be a
                     string rather than a tuple, in which case will used be for both plural and singular forms.
 
         count       this is the count, used to select between the plural and singular forms.
@@ -24,7 +24,7 @@ def pluralize(txt, /, verbs=[], nouns=[], count=0):
         the formatted string
 
     Examples:
-        
+
         1.  suppose you have have a number of cars. You could use:
 
             print(pluralize("There {verb} {count} {noun}", ("are", "is"), ("things","thing"), number_of_things))
@@ -57,3 +57,56 @@ def pluralize(txt, /, verbs=[], nouns=[], count=0):
     noun = picker(count, nouns)
 
     return txt.format(verb=verb, noun=noun, count=count)
+
+
+def join(in_array, /, quoted: bool=True, separator: str=", ", conjunction:str="", oxford_comma:bool=True) -> str:
+    """
+    Joins together two or more strings, using the specified separator (default is ", "). Optionally, the last two
+    elements can be joined by a word instead of a separator (default is no); in this case, you can also add the
+    "Oxford comma" after the last but one element and before the conjunction word (default is yes).
+
+    Parameters:
+
+        :param in_array         this is a list/tuple of strings to be joined.
+
+        :param quoted           if True, strings are enclosed in quote characters (") before being joined. The
+                                default is True.
+
+        :param separator        defines the character(s) used to separate any two elements. The default is ", ".
+
+        :param conjunction      if specified as a non blank length, the last two strings will be separated with
+                                the conjunction word instead of the separator character(s). If the oxford_comma
+                                parameter is specified as True, the separator character(s) will be included. The
+                                default conjunction word is "" (so just use separator character(s).
+
+        :param oxford_comma     if a non-blank conjunction word has been specified, it will be preceded by the separator
+                                character(s) if this parameter is True. Otherwise, the separator character(s) will be
+                                omitted before the conjunction word.
+
+    Returns:
+        string result
+    """
+    quoter = lambda t: '"' + t + '"' if quoted else t
+
+    if in_array is None or len(in_array) == 0:
+        return quoter("")
+
+    array = [quoter(x) for x in in_array]
+
+    if len(array) == 1:
+        return array[0]
+
+    if len(array) == 2:
+        if conjunction:
+            return array[0] + " " + conjunction + " " + array[1]
+        else:
+            return separator.join(array)
+
+    result = separator.join(array[:-1])
+    if oxford_comma or not conjunction:
+        result += separator
+    else:
+        result += " "
+    result += conjunction + (" " if conjunction else "")
+    result += array[-1]
+    return result
---
+++ test/utils/text_utils/test_join.py
@@ -0,0 +1,91 @@
+from biglib.utils.text_utils import join
+
+
+def test_empty():
+    expect = '""'
+    actual = join([])
+    assert actual == expect
+
+    expect = ''
+    actual = join([], quoted=False)
+    assert actual == expect
+
+    expect = '""'
+    actual = join([], separator="X")
+    assert actual == expect
+
+    expect = '""'
+    actual = join([], oxford_comma=False)
+    assert actual == expect
+
+    expect = ''
+    actual = join([], quoted=False, oxford_comma=False)
+    assert actual == expect
+
+
+def test_just_one():
+    expect = '"john"'
+    actual = join(["john"])
+    assert actual == expect
+
+    expect = 'john'
+    actual = join(["john"], quoted=False)
+    assert actual == expect
+
+def test_with_two():
+    expect = '"john", "paul"'
+    actual = join(["john", "paul"])
+    assert actual == expect
+
+    expect = '"john","paul"'
+    actual = join(["john", "paul"], separator=",")
+    assert actual == expect
+
+    expect = '"john","paul"'
+    actual = join(["john", "paul"], separator=",", oxford_comma=False)
+    assert actual == expect
+
+    expect = '"john" and "paul"'
+    actual = join(["john", "paul"], conjunction="and")
+    assert actual == expect
+
+    expect = '"john" kaj "paul"'
+    actual = join(["john", "paul"], conjunction="kaj")
+    assert actual == expect
+
+    expect = 'john, paul'
+    actual = join(["john", "paul"], quoted=False)
+    assert actual == expect
+
+    expect = 'john and paul'
+    actual = join(["john", "paul"], quoted=False, conjunction="and")
+    assert actual == expect
+
+
+def test_with_three():
+    expect = '"john", "paul", "george"'
+    actual = join(("john", "paul", "george"))
+    assert actual == expect
+
+    expect = "john, paul, george"
+    actual = join(("john", "paul", "george"), quoted=False)
+    assert actual == expect
+
+    expect = 'john, paul, and george'
+    actual = join(("john", "paul", "george"), quoted=False, conjunction="and")
+    assert actual == expect
+
+    expect = 'john, paul and george'
+    actual = join(("john", "paul", "george"), quoted=False, conjunction="and", oxford_comma=False)
+    assert actual == expect
+
+    expect = 'john, paul, george'
+    actual = join(("john", "paul", "george"), quoted=False, oxford_comma=False)
+    assert actual == expect
+
+    expect = 'john, paul, george'
+    actual = join(("john", "paul", "george"), quoted=False)
+    assert actual == expect
+
+
+
